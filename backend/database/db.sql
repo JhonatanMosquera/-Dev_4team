8c6f38444f02a6385be6aa4d6604c8ab77d8255f
jhonatan <josquera10@gmail.com>
2024-10-06 23:35:48 -0500

    Areglos de sonarcloud de db.sql

--- backend/database/db.sql
+++ backend/database/db.sql
@@ -19,29 +19,29 @@ VALUES ('John Doe', 'john@example.com', 'password123');
 
 
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(20) CHECK (role IN ('admin', 'user')) DEFAULT 'user',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE users (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     email VARCHAR(100) UNIQUE NOT NULL,
+--     password VARCHAR(255) NOT NULL,
+--     role VARCHAR(20) CHECK (role IN ('admin', 'user')) DEFAULT 'user',
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
 
-CREATE TABLE courses (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    image_url VARCHAR(255),  -- URL de una imagen del curso
-    instructor_id INT REFERENCES users(id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE courses (
+--     id SERIAL PRIMARY KEY,
+--     title VARCHAR(255) NOT NULL,
+--     description TEXT NOT NULL,
+--     image_url VARCHAR(255),  -- URL de una imagen del curso
+--     instructor_id INT REFERENCES users(id),
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
 
 
-//Si los usuarios pueden inscribirse en los cursos, necesitarás una tabla que registre esta relación.
-CREATE TABLE enrollments (
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    course_id INT REFERENCES courses(id) ON DELETE CASCADE,
-    enrolled_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- //Si los usuarios pueden inscribirse en los cursos, necesitarás una tabla que registre esta relación.
+-- CREATE TABLE enrollments (
+--     id SERIAL PRIMARY KEY,
+--     user_id INT REFERENCES users(id) ON DELETE CASCADE,
+--     course_id INT REFERENCES courses(id) ON DELETE CASCADE,
+--     enrolled_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
